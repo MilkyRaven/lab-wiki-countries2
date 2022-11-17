c409753d5331926ef5a8367a7994e0242d5cad4d
Milky Raven <heartonllamas@gmail.com>
2022-11-17 20:43:34 +0100

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+      <nav className="navbar">
+          <h2> Lab WikiCountries </h2>
+      </nav>
+  )
+}
